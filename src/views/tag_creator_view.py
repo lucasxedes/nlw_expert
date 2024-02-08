5c8f3e4be014c88a5ce19e66ea4564e53b662272
prxs <prxs@pop-os.localdomain>
2024-02-08 19:05:10 -0300

    feat: Implementing View for tag creator with Http Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,15 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        responsability for intercting with HTPP
+    '''
+    def validade_anda_create(self, http_request: HttpRequest) -> HttpResponse:
+        #body = http_request.body
+        #product_code = body["product_code"]
+        # logica de regra de negocio
+        print('estou na minha view')
+        print(http_request)
+        #retorno http
+        return HttpResponse(status_code=200, body={"Hellow": "World"})
